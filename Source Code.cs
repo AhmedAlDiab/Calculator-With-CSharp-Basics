0168262cb2b0a0a16acbe9cc0a0696b17a0c6d60
AhmedAlDiab <ahmedaldiabdev@gmail.com>
2025-02-15 13:45:56 +0200

    AAD Sends Regards

---
+++ Source Code.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CalculatorWithCSharpBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("AAD Sends Regards");            
+        }
+    }
+}
